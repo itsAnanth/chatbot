0fd440edb230fabd98716564e5247294b596bc30
EJBEAN2op <danonymousarmy@gmail.com>
2022-02-24 14:51:22 +0530

    jest(*): test lancaster

--- test/stemmer.test.ts
+++ test/stemmer.test.ts
@@ -1,7 +1,16 @@
+import LancasterStemmer from "../nlp/stemmers/lancaster"
+import natural from 'natural';
 import Stemmer from "../nlp/stemmers/Stemmer"
 import AgressiveTokenizer from "../nlp/tokenizers/agressive_tokenizer"
 import Tokenizer from "../nlp/tokenizers/Tokenizer"
 
-it('stemmer', () => {
+it('tokenizer', () => {
    expect(new AgressiveTokenizer().tokenize('hello world')).toStrictEqual(['hello', 'world']);
 })
+
+it('lancaster stemmer', () => {
+    const string = 'consideration';
+    const words = new LancasterStemmer().tokenizeAndStem(string, true);
+    expect(words).toStrictEqual(['consid']);
+});
+
